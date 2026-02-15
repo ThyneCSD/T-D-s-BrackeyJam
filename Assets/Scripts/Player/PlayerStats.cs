8fda315a85c226ea21b7343d0b9affb1aae90986
Thyne <Thyneschool@gmail.com>
2026-02-15 22:42:54 +0100

    Changed the UI and fixed the oxygen and health depletion
    W Daffoet i love him again.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,37 +1,47 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float health = 100f;
+    [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float oxygen = 100f;
+    [SerializeField] private float maxOxygen = 100f;
     [SerializeField] private float oxygenDepletionSpeed = 0.01f;
-    [SerializeField] private Image blud;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float healthDepletionSpeed = 0.01f;
+    [SerializeField] private Image healthImage;
+    [SerializeField] private Image oxygenImage;
 
-    void FixedUpdate()
+    private void Update()
     {
         //right here we do the oxygen down en als je geen meer heb dan ga je lowkey dood
         oxygen -= oxygenDepletionSpeed;
         if (oxygen <= 0)
         {
-            health -= 0.05f;
+            health -= healthDepletionSpeed;
         }
         if (oxygen != 0)
         {
-            blud.transform.localScale = new Vector3(1, oxygen * 1/100, 1);
+            oxygen -= oxygenDepletionSpeed;
+        }
+        UpdateHP();
+        UpdateOxygen();
+    }
+
+    private void UpdateHP()
+    {
+        if (healthImage != null)
+        {
+            healthImage.fillAmount = (float)health / maxHealth;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateOxygen()
     {
-        
+        if (oxygenImage != null) { }
+        {
+            oxygenImage.fillAmount = (float)(oxygen / maxOxygen);
+        }
     }
-
-    
 }
