9888b46ddb31129c8c11cdd60d66e83564e5d5a4
DaffoetxD <davidvravenswaaij@gmail.com>
2026-02-15 14:03:59 +0100

    UI beginning only my scene ahh
    yes yes oonly in myhio scene moet je ff copypla doen

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float oxygen = 100f;
+    [SerializeField] private Image blud;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //oxygen -= 0.0001f;
+        oxygen -= 0.001f;
+        if (oxygen <= 0)
+        {
+            health -= 0.05f;
+        }
+        if (oxygen != 0)
+        {
+            blud.transform.localScale = new Vector3(1, oxygen * 1/100, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
